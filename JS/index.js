c3e107f5103ad84b21608766b05da62d1328bd73
hussienAbozaid <hussien.m.abozaid@gmail.com>
2025-01-22 18:02:15 +0200

    New js file

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+let x = 5
+// prompt("Enter your age: ");
+console.log(x);
+dateOfBirth = 2025 - x; // coersion
+console.log(dateOfBirth);
+console.log(typeof (x));
+dateAfter10Years = 10 + +x; // conversion
+console.log(dateAfter10Years);
+console.log(typeof (Object));
+console.log(typeof (null));
+console.log(typeof (undefined))
+// This values give false value
+let variable = (Boolean)[0, '', undefined, null, NaN];
+// console.log(Boolean(0), Boolean(''), Boolean(undefined), Boolean(null), Boolean(NaN));
+console.log(variable);
+
+// Double equal (==) mean that i am checking if the two sides of the equality function have the same quantity
+console.log(Boolean(x == 5));
+// In the triple equal (===) mean if conditioning
+console.log(Boolean(x === 5));
+
