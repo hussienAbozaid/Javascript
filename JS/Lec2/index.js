d6229fc5627020c47e9ab9c2b1b009dcc6d93879
hussienAbozaid <hussien.m.abozaid@gmail.com>
2025-01-24 01:27:48 +0200

    finding polarity of a operation

---
+++ JS/Lec2/index.js
@@ -0,0 +1,46 @@
+const x1 = 20
+// +(prompt("Enter x1: "));
+const x2 = 15
+// +(prompt("Enter x2: "));
+const addition = x1 + x2;
+const subtraction = x1 - x2;
+
+if (x1 % 2 && x2 % 2)
+    console.log("Both even");
+else if (!(x1 % 2) && (x1 % 2) || (x1 % 2) && !(x1 % 2))
+    console.log("Both odd");
+
+// console.log(`Addition: ${addition}`);
+// console.log(`Subtraction: ${subtraction}`);
+// console.log(`Add mod: ${addition % 2}`);
+// console.log(`Subtract mod: ${subtraction % 2}`);
+
+// if (addition % 2 && subtraction % 2)
+//     console.log("Both odd");
+// else if (addition % 2 && !(subtraction % 2))
+//     console.log("addition is odd , and subtraction is even");
+// else if (!(addition % 2) && (subtraction % 2))
+//     console.log("addition is even , and subtraction is odd");
+// else
+//     console.log("both are even");
+
+// if (true) {
+//     console.log("data");
+// }
+// else if (false) {
+//     console.log("data 1")
+// }
+// else {
+//     console.log("data 2");
+// }
+
+
+
+
+
+
+
+
+
+
+
